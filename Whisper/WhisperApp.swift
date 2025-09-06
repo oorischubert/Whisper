df8a1b31c982446dbc2a948b0115f4efa52cd5fa
Oori Schubert <oorischubert@gmail.com>
2025-09-07 00:40:03 +0300

    Initial Commit

---
+++ Whisper/WhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperApp.swift
+//  Whisper
+//
+//  Created by Oori Schubert on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
